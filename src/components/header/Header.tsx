c8623ee26779e57e79d26a2dd2a8f1646fcfe77a
zhuzhuxia480 <742491842@qq.com>
2024-11-11 21:56:41 +0800

    add searchbar

--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -9,17 +9,33 @@ const Header = () => {
     return (
         <div className="w-full h-20 bg-amazon_blue text-lightText sticky top-0 z-50">
             <div className="h-full w-full mx-auto inline-flex items-center justify-between gap-1 mdl:gap-3 px-4">
-                <div className="px-2 border border-transparent hover:border-white cursor-pointer duration-500 items-center
-              justify-center h-[70%]">
+                {/*logo*/}
+                <div
+                    className="px-2 border border-transparent hover:border-white cursor-pointer duration-500 items-center justify-center h-[70%]">
                     <Image className="w-28 object-cover" src={logo} alt="logo"></Image>
                 </div>
-                <div className="px-2 border border-transparent hover:border-white cursor-pointer duration-500 items-center
-              justify-center h-[70%]">
+                {/*deliver*/}
+                <div
+                    className="px-2 border border-transparent hover:border-white cursor-pointer duration-500 items-center justify-center h-[70%]">
                     <CiLocationOn/>
                     <div className="text-xs">
                         <p>Deliver to</p>
                         <p className="text-white font-bold uppercase">US</p>
                     </div>
+                </div>
+                {/*search*/}
+                <div className="flex-1 h-10 hidden md:inline-flex items-center justify-between relative">
+                    <input
+                        className="w-full h-full rounded-md px-2 placeholder:text-sm text-black border-[3px] border-transparent focus:outline-none focus:border-amazon_yellow"
+                        type="text" placeholder="Search amazon products"/>
+                    <span
+                        className="w-12 h-full bg-amazon_yellow text-black text-3xl flex items-center justify-center absolute right-0 rounded-md rounded-br-md">
+                        <HiOutlineSearch/>
+                    </span>
+                </div>
+                {/*signin*/}
+                <div>
+
                 </div>
             </div>
         </div>
@@ -27,3 +43,15 @@ const Header = () => {
 }
 
 export default Header;
+
+
+
+
+
+
+
+
+
+
+
+
